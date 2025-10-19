aaba080b4b992bd546e7928f6d2ed2d097462407
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-10-19 10:25:49 +0200

    feat[components/input]: test for input

---
+++ src/components/Input/input.spec.tsx
@@ -0,0 +1,18 @@
+import {render, screen } from '@testing-library/react-native'
+import { Input } from '.'
+
+describe("Component: Input",()=>{
+test("Should render without activity indicator",()=>{
+   render(<Input />)
+   const activityIndicator = screen.queryByTestId('activity-indicator')
+    
+   expect(activityIndicator).toBeNull()
+})
+
+test('if render with activity indicator',()=>{
+    render(<Input />)
+   const activityIndicator = screen.getByTestId('activity-indicator')
+
+   expect(activityIndicator).toBeTruthy()
+})
+})
