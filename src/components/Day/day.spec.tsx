5cd50092cd0abddb9526cb0adbf128f5120b3c78
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-10-22 10:22:14 +0200

    feat[component/Day]: test if can render day

---
+++ src/components/Day/day.spec.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react-native"
+import { Day } from "."
+import clearDay from '@assets/clear_day.svg'
+describe('Component: Day', ()=>{
+    test('if can render day', ()=>{
+       render(<Day 
+            data={{
+            day:'18/07', 
+            min:'30°c', 
+            max:'34°c', 
+            icon: clearDay, 
+            weather:'Céu limpo'}} 
+            />)
+       expect(screen.getByText('18/07')).toBeTruthy()
+    })
+})
