08bcfcbcda2a1738f76b3fd638e4d4ed1f7e32c5
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-10-23 14:29:03 +0200

    feat[utils]: get next day specs

---
+++ src/utils/getNextDays.spec.ts
@@ -0,0 +1,10 @@
+import { getNextDays } from "./getNextDays"
+
+describe('getNextDays',()=>{
+    test('should return the next five days', ()=>{
+    const days = getNextDays()
+    
+    expect(days.length).toBe(5)
+})
+
+})
