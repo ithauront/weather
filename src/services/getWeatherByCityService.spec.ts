26bac3b400569e5083b6e9f0d11ac92b7ef5c9fa
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-10-23 14:29:34 +0200

    feat[services]: getwheaterByCity

---
+++ src/services/getWeatherByCityService.spec.ts
@@ -0,0 +1,13 @@
+import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherApiResponse"
+import { api } from "./api"
+import { getWeatherByCityService } from "./getWeatherByCityService"
+
+describe('Service: getWeatherByCityService', ()=>{
+    test('if return wheater api data formated', async ()=>{
+        jest.spyOn(api, 'get').mockResolvedValue({data: mockWeatherAPIResponse})
+
+        const response = await getWeatherByCityService({latitude: 123, longitude: 456})
+
+        expect(response).toHaveProperty('today')
+    })
+})
