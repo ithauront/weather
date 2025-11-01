b00eb946599e79ad74d134b8a5c08c3e08b98101
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-11-01 08:56:03 +0100

    feat[screens/dashboard]: test if show city weather and if can show another city weather

---
+++ src/screens/Dashboard/dashboard.spec.tsx
@@ -0,0 +1,67 @@
+import { act, fireEvent, render, screen, waitFor, waitForElementToBeRemoved } from "@__tests__/utils/customRender"
+import { Dashboard } from "."
+import { api } from "@services/api"
+import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherApiResponse"
+import { mockCityApiResponse } from "@__tests__/mocks/api/mockCityApiResponse"
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage"
+
+describe('Screen: Dashboard', ()=>{
+          const city = {
+            id: '1',
+            name: 'Rio do Sul, BR',
+            latitude: 123,
+            longitude: 456
+        }
+
+        beforeAll(async()=>{
+            await saveStorageCity(city)
+        })
+
+    test('if show city wheater', async()=>{
+        jest.spyOn(api, 'get').mockResolvedValue({data: mockWeatherAPIResponse})
+
+   
+
+
+        render(<Dashboard />)
+
+        const cityName = await waitFor(()=>screen.findByText(/rio do sul/i))
+        const forecast = await waitFor(()=>screen.findByText(/Céu limpo/i)) 
+        
+
+        expect(cityName).toBeTruthy()
+        expect(forecast).toBeTruthy()
+
+    })
+
+    test('if can show another city selected weather', async()=>{
+
+        jest.spyOn(api, 'get')
+        .mockResolvedValueOnce({data: mockWeatherAPIResponse})
+        .mockResolvedValueOnce({data: mockCityApiResponse})
+        .mockResolvedValueOnce({data: mockWeatherAPIResponse})
+
+       render(<Dashboard />)
+
+        await waitForElementToBeRemoved(()=>screen.getByTestId('loading'))
+
+        const cityName = 'São Paulo'
+
+        await waitFor(()=>act(()=>{
+            const search = screen.getByTestId('search-input')
+            fireEvent.changeText(search, cityName)
+        }))
+
+        await waitFor(()=>act(()=>{
+            fireEvent.press(screen.getByText(cityName, {exact: false}))
+        }))
+
+        const newCity = await screen.findByText(/são paulo/i)
+        const forecast = await screen.findByText(/Céu limpo/i)
+
+        expect(newCity).toBeTruthy()
+        expect(forecast).toBeTruthy()
+
+    })
+
+})
